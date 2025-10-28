6af4c29824efaa51c31e10a4e5b41202bc740542
Rishibala <rishi143bala@gmail.com>
2025-10-28 12:47:51 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Kalm_ChatBot/', // 👈 This must match your GitHub repo name
+  base: 'jarvis', // 👈 This must match your GitHub repo name
   plugins: [react()],
 });
